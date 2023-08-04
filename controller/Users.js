336a1fb2b5abef84f37516e9c64343306fc21275
aidandf29 <muhammad.aidan@ui.ac.id>
2023-08-04 20:51:43 +0700

    first commit

---
+++ controller/Users.js
@@ -0,0 +1,124 @@
+import Users from "../model/UserModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await Users.findAll({
+      attributes: ["id", "name", "email"],
+    });
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const Register = async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  if (password !== confirmPassword)
+    return res.status(400).json({ msg: "password tidak cocok" });
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
+  try {
+    await Users.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+    });
+    res.json({ msg: "Register Success" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    const user = await Users.findAll({
+      where: {
+        email: req.body.email,
+      },
+    });
+    const match = await bcrypt.compare(req.body.password, user[0].password);
+    if (!match) return res.status(400).json({ msg: "wrong password" });
+    const userId = user[0].id;
+    const name = user[0].name;
+    const email = user[0].email;
+    const accessToken = jwt.sign(
+      { userId, name, email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "30s",
+      }
+    );
+    const refreshToken = jwt.sign(
+      { userId, name, email },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+    await Users.update(
+      { refresh_token: refreshToken },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
+  } catch (error) {
+    res.status(404).json({ msg: "email not found" });
+  }
+};
+
+export const UpdateUser = async (req, res) => {
+  try {
+    const user = await Users.findAll({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    const userId = user[0].id;
+
+    const name_update = "danai";
+    await Users.update(
+      { name: name_update },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    res.json({ msg: "Update Success" });
+    return res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const Logout = async (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) return res.sendStatus(204);
+  const user = await Users.findAll({
+    where: {
+      refresh_token: refreshToken,
+    },
+  });
+  if (!user[0]) return res.sendStatus(204);
+  const userId = user[0].id;
+  await Users.update(
+    { refresh_token: null },
+    {
+      where: {
+        id: userId,
+      },
+    }
+  );
+  res.clearCookie("refreshToken");
+  return res.sendStatus(200);
+};
